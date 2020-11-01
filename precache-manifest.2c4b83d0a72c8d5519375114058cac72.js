0612f5142a9d487f90859b1c15a5dfa86c7d33ef
chloe <chloechantelle@users.noreply.github.com>
2020-11-01 19:50:46 +1000

    Updates

---
+++ precache-manifest.2c4b83d0a72c8d5519375114058cac72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cc09204d7fe76aa6872191e304eac9c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c1fa035cac5fcf9aa3b8",
+    "url": "/portfolio/static/css/main.8487d391.chunk.css"
+  },
+  {
+    "revision": "1d411ee13cec0824c2a7",
+    "url": "/portfolio/static/js/2.8626492a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.8626492a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1fa035cac5fcf9aa3b8",
+    "url": "/portfolio/static/js/main.f602d89e.chunk.js"
+  },
+  {
+    "revision": "b37b5bb5a98282a2f0e7",
+    "url": "/portfolio/static/js/runtime-main.b449097c.js"
+  }
+]);
