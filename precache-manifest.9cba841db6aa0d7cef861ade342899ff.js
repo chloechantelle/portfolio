cc62a099ffb66296001d688a07d49e43c6c08a1c
chloe <chloechantelle@users.noreply.github.com>
2021-01-07 22:15:14 +1000

    Updates

---
+++ precache-manifest.9cba841db6aa0d7cef861ade342899ff.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32fbc06d39c464e6c306de87365949c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f51fd257c69ecc58f88",
+    "url": "/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "84f923b8eb15e919750b",
+    "url": "/static/css/main.3e92ce0d.chunk.css"
+  },
+  {
+    "revision": "5f51fd257c69ecc58f88",
+    "url": "/static/js/2.aa26e823.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.aa26e823.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84f923b8eb15e919750b",
+    "url": "/static/js/main.c68583f1.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "c265055be5ae19b177fbe833a9de773e",
+    "url": "/static/media/Arame.c265055b.ttf"
+  },
+  {
+    "revision": "69e7294add43dba885c17bae2cd00a17",
+    "url": "/static/media/Snow.69e7294a.gif"
+  },
+  {
+    "revision": "d282e441a1099497bdd455df6fee6cd3",
+    "url": "/static/media/UninspiredOrdinary.d282e441.png"
+  },
+  {
+    "revision": "237bfc39fe83d44903aba18e8c90f290",
+    "url": "/static/media/Valerie.237bfc39.gif"
+  },
+  {
+    "revision": "8dec5947832eb7583b0d1f5a8c4695b7",
+    "url": "/static/media/W10DN.8dec5947.gif"
+  },
+  {
+    "revision": "9d44832630795bd8b4da33f5c7d634a4",
+    "url": "/static/media/completionist.9d448326.gif"
+  },
+  {
+    "revision": "277b60c92886597b888f00ce02877ffb",
+    "url": "/static/media/cyberpunk.277b60c9.webm"
+  },
+  {
+    "revision": "82282e8e8697c09efe959b3a643d6ba2",
+    "url": "/static/media/favi-pink.82282e8e.png"
+  },
+  {
+    "revision": "e81f4158cbd6ed39814b6af6ab094769",
+    "url": "/static/media/guide.e81f4158.png"
+  },
+  {
+    "revision": "4718e00037fac54febaaaadb7cb96155",
+    "url": "/static/media/overwatch.4718e000.mp4"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.4cb855be793af0d47f24581d2099e513.js"
+  "/precache-manifest.9cba841db6aa0d7cef861ade342899ff.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
